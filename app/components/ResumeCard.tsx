c6f65b86f56439243e9c3a89ee038bca81c2adac
anir897 <harshitsmith1@gmail.com>
2025-11-02 16:53:18 +0530

    Authentication

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import type { ReactNode } from "react";
+import ScoreCircle from "./ScoreCircle";
 
 
-const ResumeCard: React.FC<{ resume: Resume }> = ({ resume }) => {
+const ResumeCard: React.FC<{ resume: Resume ; feedback: Feedback ; imagePath : string}> = ({ resume,feedback,imagePath ,}) => {
   return (
    
-        <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000">
-            <div className="flex flex-col gap-2">
-                <h2 className="text-black font-bold break-words">
+        <Link to={`/resume/${resume.id}`} className="flex-col gap-2 resume-card animate-in fade-in duration-1000">
+            <div className="resume-card-header flex">
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-black font-bold break-word">
                     {resume.companyName}
-                </h2>
-                <h3 className="text-lg break-words text-gray-500">
+                    </h2>
+                    <h3 className="text-lg break-word text-gray-500">
                     {resume.jobTitle}
-                </h3>
+                    </h3>
+                </div>
+                <div className="shrink-0 ">
+                    <ScoreCircle score={feedback.overallScore}/>
+
+                </div>
             </div>
-            <div className="flex-shrink-0 ">
-                
+            <div className="gradient-border animate-in fade-in duration-1000">
+                <div className="w-full h-full ">
+                    <img src={imagePath} alt="resume" className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"/>
+                </div>
             </div>
+            
         </Link>
   );
 };
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,6 +4,10 @@ import {resumes} from "../../constants";
 import type { ReactNode } from "react";
 import React from "react";
 import ResumeCard from "../components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import {useLocation, useNavigate} from "react-router";
+
 
 
 
@@ -20,23 +24,37 @@ export default function Home() {
     throw new Error("Function not implemented.");
   }
 
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+  const {auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+  return <main className="bg-[url('/images/bg-home.jpg')] bg-cover">
     <Navbar/>
-    <section className="main-section">
+    {}
+    <section className="main-section py-16">
       <div className="page-heading">
         <h1>Apply Smart, Track Easy & Get Hired Faster.</h1>
         <h2>Har Application Pe Nazar, Har Resume Pe Sudhaar.</h2>
 
       </div>
-    </section>
+   
   {resumes.length > 0 && (
-  <div className="resume-section">
+  <div className="resume-section flex flex-row gap-2">
     {resumes.map((resume) => (
-      <ResumeCard key={resume.id} resume={resume} />
+      <ResumeCard
+  key={resume.id}
+  resume={resume}
+  feedback={resume.feedback}
+  imagePath={resume.imagePath}
+/>
     ))}
   </div>
 )}
 
-
+ </section>
   </main>;
 }
